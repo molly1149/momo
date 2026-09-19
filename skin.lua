d5139bd195e0f11f41b822bca3e3472ce8075a06
molly1149 <114986227@qq.com>
2026-09-19 13:41:19 +0800

    Update skin.lua

--- skin.lua
+++ skin.lua
@@ -12,7 +12,7 @@
                 if uid and uid ~= 0 then pid = tostring(uid) end
             end
         end)
-        _modSavePathCache = "/storage/emulated/0/Android/data/com.pubg.imobile/files/CHETAN_AddOutfit_Save_" .. pid .. ".txt"
+       _modSavePathCache = "/storage/emulated/0/Android/data/com.vng.pubgmobile/files/ok" .. pid .. ".txt"
         return _modSavePathCache
     end
     local function _getOutfitSavePath()
